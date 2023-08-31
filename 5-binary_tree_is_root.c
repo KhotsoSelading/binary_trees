4e8e35e848ab5de8c91b8e6165d0d812d42269c9
KhotsoSelading <k.selading007@gmail.com>
2023-08-31 14:23:05 +0200

    0x1D. C - Mandatory Task

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -8,8 +8,8 @@
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node == NULL)
+	if (node->parent == NULL|| node == NULL)
 		return (0);
 
-	return (node->parent == NULL);
+	return (1);
 }
