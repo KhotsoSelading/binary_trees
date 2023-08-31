f247584115e89b487612d67616bb2a5865189258
KhotsoSelading <k.selading007@gmail.com>
2023-08-31 22:41:48 +0200

    0x1D. C - Advanced Task

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,6 +1,29 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_depth - A function that measures the depth
+ *                     of a node in a binary tree.
+ *
+ * @tree: Pointer to the node to measure the depth.
+ *
+ * Return: The depth of the node.
+ *         Alternatively, 0 if the node is NULL.
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth;
+
+	if (tree == NULL)
+		return (0);
+
+	for (depth = 0; tree->parent != NULL; depth++)
+		tree = tree->parent;
+
+	return (depth);
+}
+
+
+/**
  * binary_trees_ancestor - A function that finds the lowest
  *                         common ancestor of two nodes.
  *
@@ -13,19 +36,23 @@
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 									const binary_tree_t *second)
 {
-	binary_tree_t *l_lca, *r_lca;
+	int depth_first = 0, depth_second =;
 
 	if (first == NULL || second == NULL)
-		return (NULL);
+		return NULL;
 
-	if (first == second)
-		return ((binary_tree_t *)first);
+	depth_first = binary_tree_depth(first);
+	depth_second = binary_tree_depth(second);
 
-	l_lca = binary_trees_ancestor(first, second->parent);
-	r_lca = binary_trees_ancestor(first->parent, second);
+	for (; depth_first > depth_second; depth_first--)
+		first = first->parent;
 
-	if (l_lca != NULL && r_lca != NULL)
-		return ((binary_tree_t *)first);
+	for (; depth_second > depth_first; depth_second--)
+		second = second->parent;
 
-	return ((l_lca != NULL) ? l_lca : r_lca);
+	for (; first != second; first = first->parent, second = second->parent)
+	{
+	}
+
+	return (binary_tree_t *)first;
 }
