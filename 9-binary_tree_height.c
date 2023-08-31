44bbbf45df8a09cf7e8040242f6eadc7d1c0bc0e
KhotsoSelading <k.selading007@gmail.com>
2023-08-31 14:49:51 +0200

    0x1D. C - Mandatory Task

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,7 +10,7 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	/*size_t l_height = 0, r_height = 0;
+	size_t l_height = 0, r_height = 0;
 
 	if (tree == NULL)
 		return (0);
@@ -18,15 +18,5 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	l_height = binary_tree_height(tree->left);
 	r_height = binary_tree_height(tree->right);
 
-	return (1 + (l_height > r_height ? l_height : r_height));
-	*/
-	if (tree == NULL)
-		return (0);
-
-	if (tree)
-	{
-		l_height = binary_tree_height(tree->left);
-		r_height = binary_tree_height(tree->right);
-		return (1 + (l_height > r_height ? l_height : r_height));
-	}
+	return (1 + ((l_height > r_height) ? l_height : r_height));
 }
