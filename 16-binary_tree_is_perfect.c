1417d81a1d1c1e4bdd6d0c9ae59d40b458bab361
KhotsoSelading <k.selading007@gmail.com>
2023-08-31 16:39:41 +0200

    0x1D. C - Mandatory Task

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -31,7 +31,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t l_height = 0, r_height 0;
+	size_t l_height = 0, r_height = 0;
 
 	if (tree == NULL)
 		return (0);
@@ -39,15 +39,12 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	l_height = binary_tree_height(tree->left);
 	r_height = binary_tree_height(tree->right);
 
-	if (l_height == rt_height)
+	if (l_height == r_height)
 	{
 		if (tree->left == NULL && tree->right == NULL)
-		{
 			return (1);
-		}
-
 		return (binary_tree_is_perfect(tree->left) &&
-		binary_tree_is_perfect(tree->right));
+				binary_tree_is_perfect(tree->right));
 	}
 
 	return (0);
